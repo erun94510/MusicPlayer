0cfad0df070fa9aba87bd606c44440aba391aacd
erun94510 <erun9414@icloud.com>
2022-07-31 03:21:10 +0900

    Add Button func

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -13,12 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any addtional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton){
+        
+        print("button tapped")
+        
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        print("slider value changed")
+        
+    }
+    
+    
 
 }
 
